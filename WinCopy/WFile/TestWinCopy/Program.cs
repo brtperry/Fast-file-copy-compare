27b1494782c7a3362487e4623d5e745bbafc030e
brtperry <brett386@gmail.com>
2015-07-01 09:13:45 +0100

    Files in solution
    All the Visual Studio source files.

---
+++ WinCopy/WFile/TestWinCopy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFile;
+
+//using WFile;
+
+namespace TestWinCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string a = @"C:\Test\a\";
+
+            const string b = @"C:\Test\b\";
+
+            var wfc = new Collector();
+
+
+
+            //var src = wfc.c
+
+
+        }
+    }
+}
---
+++ WinCopy/WFile/WFile/Copy.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using FILETIME = System.Runtime.InteropServices.ComTypes.FILETIME;
+using System.IO;
+using System.Threading;
+
+namespace WFile
+{
+    interface IException
+    {
+        event Action<Exception> OnError;
+    }
+
+    interface ICopy
+    {
+        event Action<int, int> OnBytesCopied;
+
+        event Action<int> OnProgressStep;
+    }
+
+    interface ICompared
+    {
+        event Action<List<Item>> OnCompared;
+
+        void Match();
+    }
+
+    #region Copy
+
+        public class Copy : ICopy, IException
+        {
+            public event Action<Exception> OnError;
+
+            public event Action<int, int> OnBytesCopied;
+
+            public event Action<int> OnProgressStep;
+
+            [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool CopyFileEx(string lpExistingFileName, string lpNewFileName, CopyProgressRoutine lpProgressRoutine, IntPtr lpData, ref Int32 pbCancel, CopyFileFlags dwCopyFlags);
+
+            delegate CopyProgressResult CopyProgressRoutine(long totalFileSize, long totalBytesTransferred, long streamSize, long streamBytesTransferred, uint dwStreamNumber, CopyProgressCallbackReason dwCallbackReason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData);
+
+            private int cancel;
+
+            private readonly CancellationTokenSource cancelTrigger;
+
+            private readonly string destination;
+
+            enum CopyProgressResult : uint
+            {
+                //Continue the copy operation
+                ProgressContinue = 0,
+            }
+
+            enum CopyProgressCallbackReason : uint
+            {
+                CallbackChunkFinished = 0x00000000,
+                CallbackStreamSwitch = 0x00000001
+            }
+
+            [Flags]
+            enum CopyFileFlags : uint
+            {
+                CopyFileRestartable = 0x00000002,
+                CopyFileAllowDecryptedDestination = 0x00000008
+            }
+
+            public Copy(string to)
+            {
+                cancelTrigger = new CancellationTokenSource();
+
+                destination = to;
+            }
+
+            public void Cancel(object sender, EventArgs e)
+            {
+                cancelTrigger.Cancel();
+            }
+
+            public virtual void PerformAction(List<Item> items)
+            {
+                const string slashes = "\\";
+
+                var anyOf = slashes.ToCharArray();
+
+                var counter = items.Count;
+
+                foreach (var item in items)
+                {
+                    if (cancelTrigger.IsCancellationRequested)
+                    {
+                        return;
+                    }
+
+                    if (!Directory.Exists(item.Destination))
+                    {
+                        Directory.CreateDirectory(item.Destination);
+                    }
+
+                    item.Destination = destination + item.Name;
+
+                    var itemToCopy = item;
+
+                    try
+                    {
+                        Parallel.Invoke(() => WCopy(itemToCopy));
+
+                        if (OnProgressStep != null)
+                        {
+                            OnProgressStep(--counter);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        OnError(ex);
+                    }
+                }
+            }
+
+            private void WCopy(Item item)
+            {
+                CopyFileEx(item.Source, item.Destination, CopyProgressHandler, IntPtr.Zero, ref cancel, CopyFileFlags.CopyFileRestartable | CopyFileFlags.CopyFileAllowDecryptedDestination);
+            }
+
+            private CopyProgressResult CopyProgressHandler(long total, long transferred, long streamSize, long streamByteTrans, uint dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData)
+            {
+                switch (reason)
+                {
+                    case CopyProgressCallbackReason.CallbackStreamSwitch:
+
+                        break;
+
+                    case CopyProgressCallbackReason.CallbackChunkFinished:
+
+                        if (OnBytesCopied != null)
+                        {
+                            OnBytesCopied((int)total, (int)transferred);
+                        }
+
+                        break;
+                }
+
+                return CopyProgressResult.ProgressContinue;
+            } 
+        }
+
+    #endregion
+
+    #region Collector
+
+        public class Collector
+        {
+            public virtual IEnumerable<Item> Collect(string folder)
+            {
+                var results = new List<Item>();
+
+                Recurse(folder, ref results);
+
+                return results;
+            }
+
+            #region WIN_32 : Recursive directory functions
+
+            /// <summary>
+            /// Recursive function to populate a MyImage list.
+            /// </summary>
+            /// <param name="directory"></param>
+            /// <param name="level"></param>
+            /// <param name="lmi"></param>
+            private void Recurse(string directory, ref List<Item> lmi)
+            {
+                //IntPtr
+                var invalidHandleValue = new IntPtr(-1);
+
+                Win32FindData findData;
+
+                var findHandle = FindFirstFile(@directory + @"\*", out findData);
+
+                if (findHandle == invalidHandleValue) return;
+
+                do
+                {
+                    if ((findData.dwFileAttributes & FileAttributes.Directory) != 0)
+                    {
+                        if (findData.cFileName != "." && findData.cFileName != "..")
+                        {
+                            var subdirectory = directory + (directory.EndsWith(@"\") ? "" : @"\") + findData.cFileName;
+
+                            Recurse(subdirectory, ref lmi);
+                        }
+                    }
+                    else
+                    {
+                        if (findData.cFileName != "Thumbs.db")
+                        {
+                            const string SLASH = @"\\";
+
+                            var hFt2 = (((long)findData.ftLastWriteTime.dwHighDateTime) << 32) | ((uint)findData.ftLastWriteTime.dwLowDateTime);
+
+                            var place = directory;
+
+                            var itm = new Item();
+
+                            itm.Name = findData.cFileName;
+
+                            itm.Destination = directory.Remove(0, directory.LastIndexOf(@"\")) + SLASH + itm.Name;
+
+                            itm.Source = directory + SLASH + itm.Name;
+
+                            itm.LastWriteTime = DateTime.FromFileTime(hFt2);
+
+                            lmi.Add(itm);
+
+                            //lmi.Add(new Item
+                            //{
+                            //    Destination = Destination = place.Remove(0, directory.LastIndexOf(@"\")),
+
+                            //    Name = findData.cFileName,
+
+                            //    Source = place,
+
+                            //    LastWriteTime = DateTime.FromFileTime(hFt2)
+                            //});
+                        }
+                    }
+                }
+                while (FindNextFile(findHandle, out findData));
+
+                FindClose(findHandle);
+            }
+
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+            public struct Win32FindData
+            {
+                public readonly FileAttributes dwFileAttributes;
+                private readonly FILETIME ftCreationTime;
+                private readonly FILETIME ftLastAccessTime;
+                public readonly FILETIME ftLastWriteTime;
+                private readonly uint nFileSizeHigh; //changed all to uint from int, otherwise you run into unexpected overflow
+                private readonly uint nFileSizeLow;  //| http://www.pinvoke.net/default.aspx/Structures/WIN32_FIND_DATA.html
+                private readonly uint dwReserved0;   //|
+                private readonly uint dwReserved1;   //v
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+                public readonly string cFileName;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 14)]
+                private readonly string cAlternate;
+            }
+
+            [DllImport("kernel32", SetLastError = true)]
+            static extern bool FindClose(IntPtr hndFindFile);
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            private static extern IntPtr FindFirstFile(string lpFileName, out Win32FindData lpFindFileData);
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            private static extern bool FindNextFile(IntPtr hFindFile, out Win32FindData lpFindFileData);
+
+            #endregion
+        }
+
+    #endregion
+
+    #region Comparer
+
+        public class Compare : ICompared, IException 
+        {
+            public event Action<Exception> OnError;
+
+            public event Action<List<Item>> OnCompared;
+
+            private readonly IEnumerable<Item> source;
+
+            private readonly IEnumerable<Item> destination;
+
+            public Compare(IEnumerable<Item> src, IEnumerable<Item> dest)
+            {
+                source = src;
+
+                destination = dest;
+            }
+
+            public async virtual void Match()
+            {
+                try
+                {
+                    await Task.Run(() => OnCompared(source.Except(destination, new EqualityComparer()).ToList()));
+                }
+                catch (AggregateException ae)
+                {
+                    ae.Handle((x) => 
+                    {
+                        //
+                        // It's likely that the only exception would be ArgumentNullException, this I
+                        // can handle, anything else it's wierd.
+                        // 
+                        if (x is ArgumentNullException)
+                        {
+                            OnError(x);
+                        }
+
+                        return false;
+                    });
+                }
+            }
+        }
+
+    #endregion
+
+    #region Item
+
+        public class Item
+        {
+            public string Name;
+
+            public string Destination;
+
+            public string Source;
+
+            public DateTime LastWriteTime;
+
+            public override string ToString()
+            {
+                return string.Format("{ Name : {0}, Source : {1}, Destination : {2} }", Name, Source, Destination);
+            }
+        }
+
+    #endregion
+
+    #region EqualityComparer
+
+        public class EqualityComparer : IEqualityComparer<Item>
+        {
+            public virtual bool Equals(Item x, Item y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+
+                return x != null && y != null && x.Name.Equals(y.Name) && x.LastWriteTime == y.LastWriteTime;
+            }
+
+            public int GetHashCode(Item obj)
+            {
+                var hash = obj.Name == null ? 0 : obj.Name.GetHashCode();
+
+                var lwt = obj.LastWriteTime.GetHashCode();
+
+                //Calculate the hash code for the item.  
+                return hash ^ lwt;
+            }
+        }
+
+    #endregion
+
+
+    #region
+    #endregion
+
+}
